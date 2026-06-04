eb9ac7892b3f1bafc9cd97a8e33938726b0c7341
Krishankant89 <toxickush90@gmail.com>
2026-06-04 12:13:07 +0530

    Bubble sort algo

--- Sorting algorithms/Bubble_sort.py
+++ Sorting algorithms/Bubble_sort.py
@@ -1,2 +1,13 @@
-def binary_search():
+def bubble_sort(arr):
     
+    n = len(arr)
+    
+    for i in range(n):
+        for j in range(0, n - i -1):
+            if arr[j] > arr[j +1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                
+    return arr
+
+nums = [24,11,45,67,56,6]
+print(bubble_sort(nums))
