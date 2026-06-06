68b3bc496ef12427da8369cf51fbc49352585f55
Krishankant89 <toxickush90@gmail.com>
2026-06-07 00:13:34 +0530

    selection sort algo code

---
+++ Sorting algorithms/selection_sort.py
@@ -0,0 +1,17 @@
+def insertion_sort(arr):
+    n = len(arr)
+    
+    for i in range(n -1):
+        min_index = i
+        
+        for j in range(i + 1,n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+                
+        arr[i], arr[min_index] = arr[min_index],arr[i]
+        
+    return arr
+
+arr = [23,11,45,2]
+fun = insertion_sort(arr)
+print(fun)
