83af31a96c7e71ac0f552fe128d74cb597a2a6fd
Krishankant89 <toxickush90@gmail.com>
2026-06-08 19:55:57 +0530

    pattern printing

---
+++ Pattern/decreasing_pyramind_pattern.py
@@ -0,0 +1,7 @@
+n = 4
+for i in range(n):
+    num = 1
+    for j in range(n-i):
+        print(num ,end=" ")
+        num +=1
+    print()
---
+++ Pattern/same_number_increasing.py
@@ -0,0 +1,7 @@
+n = 4
+for i in range(n):
+    i = 1
+    for j in range(n+1):
+        print(i,end="")
+        i += 1
+    print()
