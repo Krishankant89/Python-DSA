2b86c895813f32bf35a0c98a6f7b34bc195b3136
Krishankant89 <toxickush90@gmail.com>
2026-06-12 12:21:02 +0530

    neetcode easy solution

---
+++ Neetcode/Valid_anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        s = sorted(s)
+        t = sorted(t)
+
+        return s == t
+
+if __name__ == "__main__":
+    s = "racecar"
+    t = "carrace"
+    solution = Solution()
+    if(solution.isAnagram(s,t)):
+        print("True")
+    else:
+        print("False")
