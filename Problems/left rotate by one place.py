fec38bc1962c4adf612b4928f3cc829ce9677122
Krishankant89 <toxickush90@gmail.com>
2026-06-18 13:38:19 +0530

    left rotate array by one place

---
+++ Problems/left rotate by one place.py
@@ -0,0 +1,13 @@
+def leftrotate(arr):
+    
+    n = len(arr)
+    temp = arr[0]
+    for i in range(n - 1):
+        arr[i] = arr[i+1]
+        
+    arr[n - 1] = temp
+    
+    return arr
+
+arr = [1,2,3,4,5]
+print(leftrotate(arr))
